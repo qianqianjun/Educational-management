a8c99f264df324b2781a6ea8dda4f6dc71dc1691
孟庆强 <40706497+SpringTY@users.noreply.github.com>
2019-04-09 22:42:22 +0800

    完成了学期系统的Dao层，Service层，及其测试

--- src/main/java/buct/software/dao/SemesterDao.java
+++ src/main/java/buct/software/dao/SemesterDao.java
@@ -1,9 +1,16 @@
 package buct.software.dao;
 
+import buct.software.domain.Semester;
+
+import java.util.List;
+
 /**
  * @author 高谦
  * 学期表 semester 数据库操作类
  */
-public interface SemesterDao {
 
+public interface SemesterDao {
+    public void insertSemester(Semester semester);
+    public List<Semester> getAll();
+    public List<Semester> getSemesterMostId();
 }
---
+++ src/test/java/buct/software/ServiceTest/SemesterTest.java
@@ -0,0 +1,38 @@
+package buct.software.ServiceTest;
+
+import buct.software.domain.Semester;
+import buct.software.service.SemesterService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemesterTest {
+    @Autowired
+    SemesterService semesterService;
+    @Test
+    public void getCurrentSemesterInfo(){
+        Semester currentSemesterInfo = semesterService.getCurrentSemesterInfo();
+        System.out.println(currentSemesterInfo);
+    }
+    @Test
+    public void getCurrentSemesterId(){
+        Integer currentSemesterId = semesterService.getCurrentSemesterId();
+        System.out.println(currentSemesterId);
+    }
+    @Test
+    public void getSemesterList(){
+        List<String> semesterList = semesterService.getSemesterList();
+        System.out.println(semesterList);
+    }
+    @Test
+    public void getSemesterDomain(){
+        List<Semester> semesterList = semesterService.getSemesterDomain();
+        System.out.println(semesterList);
+    }
+}
