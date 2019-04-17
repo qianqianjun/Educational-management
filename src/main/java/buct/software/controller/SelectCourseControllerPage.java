2c86dd9c82250ddacd793cd356b5152b0f752cc1
ys11235813 <527168576@qq.com>
2019-04-18 01:01:00 +0800

    完成选课手机端

--- src/main/java/buct/software/controller/SelectCourseControllerPage.java
+++ src/main/java/buct/software/controller/SelectCourseControllerPage.java
@@ -1,16 +1,20 @@
 package buct.software.controller;
+import buct.software.domain.College;
 import buct.software.domain.SelectCourse;
 import buct.software.domain.User;
 import buct.software.service.CollegeService;
 import buct.software.service.SelectCourseService;
 import buct.software.service.SemesterService;
+import buct.software.utils.ResponseMessage;
 import buct.software.views.SelectCourseView;
+import org.apache.poi.hssf.record.ObjRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -68,6 +72,14 @@ public class SelectCourseControllerPage {
                 selectCourseService.getCourseTable(semesterId,sno);
         parMap.put("coursetable",courseTable);
         return "coursetable";
+    }
+
+    @GetMapping("/selectcoursemobile")
+    public String selectcoursemobile(Map<String, Object> parmMap){
+        ResponseMessage res=collegeService.getAllCollege();
+        List<College> collegeList=(List<College>) res.getData();
+        parmMap.put("collegelist",collegeList);
+        return "selectcoursemobile";
     }
 
 }
