0200c37c7694b3bba646387fb441f37b4bf24688
孟庆强 <40706497+SpringTY@users.noreply.github.com>
2019-04-28 00:25:16 +0800

    本地化
    # Conflicts:
    #	src/main/resources/application.properties
    #	src/main/resources/static/css/bootstrap.min.css
    #	src/main/resources/static/css/dashboard.css
    #	src/main/resources/static/js/vue.js

---
+++ src/main/java/buct/software/configuration/BackStageConfiguration.java
@@ -0,0 +1,13 @@
+package buct.software.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class BackStageConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
