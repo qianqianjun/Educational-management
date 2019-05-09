f270ea5116eacec16bbd0a4e8d7d3d8c9103a419
SEALXD <wangyilinxx@126.com>
2019-05-09 20:41:54 +0800

    完成教师移动端首页

--- src/main/java/buct/software/controller/LoginController.java
+++ src/main/java/buct/software/controller/LoginController.java
@@ -41,6 +41,8 @@ public class LoginController {
     SemesterService semesterService;
     @Autowired
     SchedulingService schedulingService;
+    @Autowired
+    CollegeService collegeService;
 
     /**
      * 登录页面网址，请求这个地址用于展现登录页面
@@ -95,13 +97,18 @@ public class LoginController {
                     System.out.println(teaCourseViews.get(i).getCourseName());
                 }
                 session.setAttribute("CourseTable",teaCourseViews);
+
+                parmMap.put("courseTable", teaCourseViews);//课表
+                Teacher teacher = teacherService.getTeacherByTno(tno);
+                parmMap.put("teainfo", teacher);
+                String cid = teacher.getCollegeId().toString();
+                String colname = collegeService.getColnameById(cid);
+                parmMap.put("colname", colname);
+                if (platform.equals("mobile"))
+                    return "MobileTeacherHome";
                 return "teacher";
             } else {
-                if (platform.equals("mobile")) {
-                    return "redirect:/GoMobileHomePage";
-                } else {
-                    return "redirect:/GoHomePage";
-                }
+               return "";
             }
         }
     }
