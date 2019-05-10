ecd3488b6baeb7615218dd311170d957202d9833
余中瑞 <2417613380@qq.com>
2019-05-10 12:13:01 +0800

    teacher

--- src/main/java/buct/software/controller/QuestionController.java
+++ src/main/java/buct/software/controller/QuestionController.java
@@ -244,6 +244,25 @@ public class QuestionController {
         }//添加学生详细信息
         map.put("choices",questionStudentChooses);
         map.put("students",students);
+
+        //判断有无选择成功
+        HttpSession session = request.getSession();
+
+        session.setAttribute("snoSured",question.getSno());
+
+        Object snoSured = session.getAttribute("snoSured");
+        int snoSuredInt;
+        if(snoSured!=null){
+            snoSuredInt = (int)snoSured;
+        }else{
+            snoSuredInt = -1;
+        }
+        boolean isSured = false;
+        if(snoSuredInt!=-1 && snoSuredInt==question.getSno()){
+            isSured = true;
+        }
+        map.put("isSured",isSured);
+
         return "TeaQuesDetails";
     }
 
@@ -262,6 +281,7 @@ public class QuestionController {
         }//添加学生详细信息
         map.put("choices",questionStudentChooses);
         map.put("students",students);
+
         return "TeaQuesDetailsMobile";
     }
 
@@ -272,6 +292,8 @@ public class QuestionController {
                               Map<String,Object>map){
         boolean isSured = questionService.sureQuestionStudent(questionid,sno);
         map.put("isSured",isSured);
+        HttpSession session = request.getSession();
+        session.setAttribute("snoSured",sno);
         return "redirect:/TeaQuesDetails?questionid="+questionid;
     }
 
